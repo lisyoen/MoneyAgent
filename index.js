94a726f7ae1e809bced708dda0af7d8b7d75a928
lisyoen <lisyoen@gmail.com>
2017-11-15 03:43:01 +0900

    static routing

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
+var express = require('express');
+
+var app = express();
+
+// using handlebar as template engine
+var handlebars = require('express-handlebars')
+  .create({defaultLayout: 'main'});
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+app.set('port', process.env.PORT || 80);
+
+app.use(express.static(__dirname + '/public'));
+
+app.use(function(req, res){
+  res.type('text/plane');
+  res.status(404);
+  res.send('404 - Not Found');
+});
+
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  res.type('text/plane');
+  res.status(500);
+  res.send('500 - Server Error');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Express started on http://localhost:' +
+  app.get('port') + '; press Ctrl-C to terminate.');
+});
+
+/*
 const sqlite3 = require('sqlite3').verbose();
 
 let db = new sqlite3.Database('./db/md.db', sqlite3.OPEN_READWRITE, (err) => {
@@ -23,3 +56,4 @@ db.close((err) => {
   }
   console.log('Close the database connection.');
 });
+*/
