46d0c27fc6d53f9b7306b6254c4a97025b1d589a
lisyoen <lisyoen@gmail.com>
2017-11-22 11:59:10 +0900

    change comment about config.public.js

--- config.public.js
+++ config.public.js
@@ -1,4 +1,4 @@
-// you have to rename this file to 'config.private.js'
+// fill information what your own service, and rename this file to 'config.private.js'.
 module.exports = {
   service_domain: 'your.service.domain',
   service_port: 80
