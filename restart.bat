576d0d56b390df7af1fa32d740e0d5678ceaadc0
lisyoen <lisyoen@gmail.com>
2017-11-28 18:56:57 +0900

    restart script modified

--- restart.bat
+++ restart.bat
@@ -1,1 +1,12 @@
-pm2 restart mas
+@if [%1] == [/p] goto PULL
+@if [%1] == [/P] goto PULL
+
+call pm2 restart mas
+goto END
+
+:PULL
+call pm2 delete mas
+git pull
+call pm2 start index.js --name mas
+
+:END
